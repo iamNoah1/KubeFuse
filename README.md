4a4c073cb2dfe0d084095f957648deba33fccaaa
Noah Ispas (iamNoah1) <noahispas@gmail.com>
2025-08-30 17:22:26 +0100

    add cobra skeleton with the main set command
    Signed-off-by: Noah Ispas (iamNoah1) <noahispas@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # KubeFuse
+
+A CLI tool that allows safe, temporary live-patching of Kubernetes resources with features like TTL-based rollback, dry-run, and audit annotations.
+
+## Key Features (MVP):
+- Patch Kubernetes resources, ie `spec.replicas=2`
+- Optional `-ttl` flag to auto-revert the change
+- Add patch reason annotations
+- Dry-run mode for previewing changes
+- Optional: TTL manager as in-cluster controller or background job
+
+## Intended Example Usage (not implemented yet)
+
+kubefuse deploy/web -n prod \\
+  set spec.template.spec.containers[0].image=nginx:1.21 \\
+  --ttl 10m \\
+  --reason "hotfix for prod"
+
+## Usage 
+* `go run main.go <command> <subcommand>`
+* or `go build -o kubefuse` and then `./kubefuse <command> <subcommand>`
---
+++ cmd/set.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2025 Noah Ispas <noahispas.public@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// setCmd represents the set command
+var setCmd = &cobra.Command{
+	Use:   "set <kind/name> <path=value> [path=value]...",
+	Short: "Patch fields on a resource with optional TTL and audit",
+	Args:  cobra.MinimumNArgs(2),
+	Long:  `Patch fields on a resource with optional TTL and audit`,
+	Run: func(cmd *cobra.Command, args []string) {
+		target := args[0]
+		patches := args[1:]
+
+		fmt.Println("target:", target)
+		fmt.Println("patches:", patches)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(setCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// setCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// setCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
